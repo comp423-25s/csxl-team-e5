17a140405c6e65e57fe6ea5a916c6976200a94e9
Daniel Henderson <dhendinfo@gmail.com>
2025-04-19 18:11:51 -0400

    course suggestion cards dialog and button created. Css still needs to be added

--- frontend/src/app/my-courses/course-seek/course-seek.component.ts
+++ frontend/src/app/my-courses/course-seek/course-seek.component.ts
@@ -28,6 +28,92 @@ export class CourseSeekComponent {
     sections: null
   });
 
+  exampleCourseData = {
+    course: {
+      id: 12345,
+      term_id: 'SP2025',
+      subject_code: 'COMP',
+      number: '426',
+      title: 'Modern Web Programming',
+      role: 'instructor',
+      sections: [
+        {
+          id: 10001,
+          number: '001',
+          meeting_pattern: 'MWF 10:00AM-10:50AM, SN 011',
+          course_site_id: 12345,
+          subject_code: 'COMP',
+          course_number: '426',
+          section_number: '001'
+        },
+        {
+          id: 10002,
+          number: '601',
+          meeting_pattern: 'Th 3:30PM-4:45PM, SN 115',
+          course_site_id: 12345,
+          subject_code: 'COMP',
+          course_number: '426',
+          section_number: '601'
+        }
+      ],
+      gtas: [
+        {
+          id: 2001,
+          onyen: 'gradta1',
+          first_name: 'Graduate',
+          last_name: 'Assistant',
+          pronouns: 'they/them',
+          email: 'gradta1@unc.edu',
+          github_avatar: 'https://github.com/avatars/gradta1.jpg',
+          github: 'gradta1',
+          bio: 'Computer Science PhD student focusing on web technologies.',
+          linkedin: 'https://linkedin.com/in/gradta1',
+          website: 'https://cs.unc.edu/~gradta1'
+        }
+      ],
+      utas: [
+        {
+          id: 3001,
+          onyen: 'undergradta1',
+          first_name: 'Undergraduate',
+          last_name: 'Assistant1',
+          pronouns: 'she/her',
+          email: 'undergradta1@unc.edu',
+          github_avatar: 'https://github.com/avatars/undergradta1.jpg',
+          github: 'undergradta1',
+          bio: 'Senior Computer Science major, passionate about teaching.',
+          linkedin: 'https://linkedin.com/in/undergradta1',
+          website: null
+        },
+        {
+          id: 3002,
+          onyen: 'undergradta2',
+          first_name: 'Undergraduate',
+          last_name: 'Assistant2',
+          pronouns: 'he/him',
+          email: 'undergradta2@unc.edu',
+          github_avatar: 'https://github.com/avatars/undergradta2.jpg',
+          github: 'undergradta2',
+          bio: 'Junior CS & Math double major who loves frontend development.',
+          linkedin: null,
+          website: 'https://undergradta2.github.io'
+        }
+      ]
+    },
+    termId: 'SP2025'
+  };
+  courseCardArray: WritableSignal<CourseSeekCourseCard[]> = signal([
+    this.exampleCourseData,
+    this.exampleCourseData,
+    this.exampleCourseData,
+    this.exampleCourseData,
+    this.exampleCourseData,
+    this.exampleCourseData,
+    this.exampleCourseData,
+    this.exampleCourseData,
+    this.exampleCourseData
+  ]);
+
   constructor(
     private resourceService: CourseSeekService,
     protected chatService: ChatService,
